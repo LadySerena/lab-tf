a6dd0633c7f0837f24f522caa2e0d3ea2740f36c
LadySerena <serena.tiede+github@gmail.com>
2020-08-22 18:59:48 -0500

    feat: add activated apis for telvanni-lab
    this enables all of the activated apis to be declared in code.

--- main.tf
+++ main.tf
@@ -11,6 +11,20 @@ module "gcs_buckets" {
   prefix = "tel"
 }
 
+module "project-services" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "4.0.0"
+
+  project_id                  = data.google_project.project.project_id
+
+  activate_apis = [
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "storage.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+  ]
+}
+
 module "lab-triggers" {
   source = "git::https://github.com/LadySerena/terraform-modules.git//github-push-pr-tag-triggers?ref=0.1.0"
   ownerName = "LadySerena"
