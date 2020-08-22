bfefcbab6bd44699202220ea9f7461565498fad8
LadySerena <serena.tiede+github@gmail.com>
2020-08-22 18:35:02 -0500

    feat: add prometheus triggers
    also makes cloud build use the terraform plan

--- main.tf
+++ main.tf
@@ -2,18 +2,27 @@ data "google_project" "project" {
 }
 
 module "gcs_buckets" {
-  source     = "terraform-google-modules/cloud-storage/google"
-  version    = "~> 1.6"
-  location   = "US"
-  project_id = data.google_project.project.id
+  source = "terraform-google-modules/cloud-storage/google"
+  version = "~> 1.6"
+  location = "US"
+  project_id = data.google_project.project.project_id
   names = [
-  "deb-repo"]
+    "deb-repo"]
   prefix = "tel"
 }
 
-module "lab-trigger" {
-  source     = "git::https://github.com/LadySerena/terraform-modules.git//github-push-pr-tag-triggers?ref=0.1.0"
-  ownerName  = "LadySerena"
-  repoName   = "lab-tf"
+module "lab-triggers" {
+  source = "git::https://github.com/LadySerena/terraform-modules.git//github-push-pr-tag-triggers?ref=0.1.0"
+  ownerName = "LadySerena"
+  repoName = "lab-tf"
+  project_id = data.google_project.project.project_id
+}
+
+module "prometheus-deb-triggers" {
+  source = "git::https://github.com/LadySerena/terraform-modules.git//github-push-pr-tag-triggers?ref=0.1.0"
+  ownerName = "LadySerena"
+  ciDevPath = "ci/feature/cloudbuild.yaml"
+  ciMainPath = "ci/release/cloudbuild.yaml"
+  repoName = "lab-tf"
   project_id = data.google_project.project.project_id
 }
