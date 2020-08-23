009dc5ecc2ef9148fb3699a2a511d205138a2e29
LadySerena <serena.tiede+github@gmail.com>
2020-08-22 19:48:27 -0500

    fix: use correct module to manage cloud build
    the project iam bindings is what manages the google managed service
    accounts eg the cloud build service account. This change allows it to
    manage project api access so that apis can be enabled via terraform.

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ data "google_project" "project" {
 }
 
 module "gcs_buckets" {
-  source = "terraform-google-modules/cloud-storage/google"
-  version = "~> 1.6"
-  location = "US"
+  source     = "terraform-google-modules/cloud-storage/google"
+  version    = "~> 1.6"
+  location   = "US"
   project_id = data.google_project.project.project_id
   names = [
-    "deb-repo"]
+  "deb-repo"]
   prefix = "tel"
 }
 
@@ -15,7 +15,7 @@ module "project-services" {
   source  = "terraform-google-modules/project-factory/google//modules/project_services"
   version = "4.0.0"
 
-  project_id                  = data.google_project.project.project_id
+  project_id = data.google_project.project.project_id
 
   activate_apis = [
     "compute.googleapis.com",
@@ -27,17 +27,31 @@ module "project-services" {
 }
 
 module "lab-triggers" {
-  source = "git::https://github.com/LadySerena/terraform-modules.git//github-push-pr-tag-triggers?ref=0.1.0"
-  ownerName = "LadySerena"
-  repoName = "lab-tf"
+  source     = "git::https://github.com/LadySerena/terraform-modules.git//github-push-pr-tag-triggers?ref=0.1.0"
+  ownerName  = "LadySerena"
+  repoName   = "lab-tf"
   project_id = data.google_project.project.project_id
 }
 
 module "prometheus-deb-triggers" {
-  source = "git::https://github.com/LadySerena/terraform-modules.git//github-push-pr-tag-triggers?ref=0.1.0"
-  ownerName = "LadySerena"
-  ciDevPath = "ci/feature/cloudbuild.yaml"
+  source     = "git::https://github.com/LadySerena/terraform-modules.git//github-push-pr-tag-triggers?ref=0.1.0"
+  ownerName  = "LadySerena"
+  ciDevPath  = "ci/feature/cloudbuild.yaml"
   ciMainPath = "ci/release/cloudbuild.yaml"
-  repoName = "prometheus-deb"
+  repoName   = "prometheus-deb"
   project_id = data.google_project.project.project_id
 }
+
+module "project-iam-bindings" {
+  source = "terraform-google-modules/iam/google//modules/projects_iam"
+  projects = [
+  data.google_project.project.project_id]
+  mode = "additive"
+
+  bindings = {
+    "roles/serviceusage.serviceUsageAdmin" = [
+      "serviceAccount:952032963423@cloudbuild.gserviceaccount.com",
+    ]
+  }
+}
+
